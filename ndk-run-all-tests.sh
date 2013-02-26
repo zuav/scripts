489e49e79422ea6d24b209df7b50f9a5359ddebd
Alexander Zhukov <zuav@yandex.ru>
2013-02-26 16:07:01 +0400

    Use started emulators.

--- ndk-run-all-tests.sh
+++ ndk-run-all-tests.sh
@@ -46,13 +46,13 @@ RESULTS_BASE_DIR=/var/tmp/ndk.tests.results/
 #RESULTS_DIR=/var/tmp/ndk.tests.results/2013.01.22-21:12:40
 RESULTS_DIR=/var/tmp/ndk.tests.results/`date +%Y.%m.%d-%H:%M:%S`
 GCC_TOOLCHAINS="4.7 4.6 4.4.3"
-CLANG_TOOLCHAINS="clang3.1"
-ARCHS="armeabi armeabi-v7a mips x86"
+CLANG_TOOLCHAINS="clang3.1 clang3.2"
+ARCHS=""
+
 
 # just exit on error
 function error_exit ()
 {
-    cd $OLD_DIR
     echo Failed!
     exit 1
 }
@@ -60,32 +60,90 @@ function error_exit ()
 mkdir -p $RESULTS_BASE_DIR || error_exit
 mkdir    $RESULTS_DIR      || error_exit
 
-for arch in $ARCHS
+# Find all devices
+DEVICE_armeabi=
+DEVICE_armeabi_v7a=
+DEVICE_mips=
+DEVICE_x86=
+
+ADB_CMD=`which adb`
+if [ -z "$ADB_CMD" ] ; then
+    echo "ERROR: adb not found"
+    exit 1
+fi
+
+# Get list of online devices, turn ' ' in device into '.'
+DEVICES=`$ADB_CMD devices | grep -v offline | awk 'NR>1 {gsub(/[ \t]+device$/,""); print;}' | sed '/^$/d' | tr ' ' '.'`
+for DEVICE in $DEVICES; do
+    # undo previous ' '-to-'.' translation
+    DEVICE=$(echo $DEVICE | tr '.' ' ')
+    # get arch
+    ARCH=`$ADB_CMD -s "$DEVICE" shell getprop ro.product.cpu.abi | tr -dc '[:print:]'`
+    case "$ARCH" in
+        armeabi-v7a)
+            DEVICE_armeabi_v7a=$DEVICE
+            ;;
+        armeabi)
+            DEVICE_armeabi=$DEVICE
+            ;;
+        x86)
+            DEVICE_x86=$DEVICE
+            ;;
+        mips)
+            DEVICE_mips=$DEVICE
+            ;;
+        *)
+            echo "ERROR: Unsupported architecture: $ARCH"
+            exit 1
+    esac
+done
+
+# check that all required devices are present
+if [ -z "$DEVICE_armeabi" ] ; then
+    echo "ERROR: not found armeabi device/emulator"
+    exit 1
+fi
+
+if [ -z "$DEVICE_armeabi_v7a" ] ; then
+    echo "ERROR: not found armeabi-v7a device/emulator"
+    exit 1
+fi
+
+if [ -z "$DEVICE_x86" ] ; then
+    echo "ERROR: not found x86 device/emulator"
+    exit 1
+fi
+
+if [ -z "$DEVICE_mips" ] ; then
+    echo "ERROR: not found mips device/emulator"
+    exit 1
+fi
+
+EMULATORS="armeabi:$DEVICE_armeabi armeabi-v7a:$DEVICE_armeabi_v7a x86:$DEVICE_x86 mips:$DEVICE_mips"
+
+for EMU in $EMULATORS
 do
-    echo "Starting emulator for $arch"
-    emulator -avd $arch &
-    adb wait-for-device
+    ARCH=${EMU%:*}
+    DEVICE=${EMU#*:}
+    echo "Starting tests for $ARCH using device $DEVICE"
     # GCC toolchains
     for toolchain in $GCC_TOOLCHAINS
     do
         echo "Running tests for toolchain GCC-$toolchain"
-        LOG_BASE=$arch-gcc-$toolchain
+        LOG_BASE=$ARCH-gcc-$toolchain
         LOG_FILE=$RESULTS_DIR/$LOG_BASE.txt
-        ./tests/run-tests.sh $FULL --abi=$arch --toolchain-version=$toolchain > $LOG_FILE
+        ANDROID_SERIAL=$DEVICE ./tests/run-tests.sh $FULL --continue-on-build-fail --abi=$ARCH --toolchain-version=$toolchain > $LOG_FILE
         cp $TESTS_BUILD_DIR/build-tests.log $RESULTS_DIR/$LOG_BASE.build-tests.log
     done
-    # Clang toolchains, currently only one
+    # Clang toolchains
     for toolchain in $CLANG_TOOLCHAINS
     do
         echo "Running tests for toolchain $toolchain"
-        LOG_BASE=$arch-$toolchain
+        LOG_BASE=$ARCH-$toolchain
         LOG_FILE=$RESULTS_DIR/$LOG_BASE.txt
-        ./tests/run-tests.sh $FULL --abi=$arch --toolchain-version=$toolchain > $LOG_FILE
+        ANDROID_SERIAL=$DEVICE ./tests/run-tests.sh $FULL --continue-on-build-fail --abi=$ARCH --toolchain-version=$toolchain > $LOG_FILE
         cp $TESTS_BUILD_DIR/build-tests.log $RESULTS_DIR/$LOG_BASE.build-tests.log
     done
-    echo "Stopping emulator for $arch"
-    adb emu kill
-    sleep 2
 done
 
 
